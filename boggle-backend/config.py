d80f11e5fbff5d5c87a9e407df3d8bfccd6bfb4a
Stephane Toyo Demanou <stephanetoyo14@gmail.com>
2025-03-29 22:50:26 -0400

    Basic Flask Setup

--- boggle-backend/config.py
+++ boggle-backend/config.py
@@ -1,1 +1,7 @@
- 
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'boggle-battle-secret'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///boggle.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    CORS_HEADERS = 'Content-Type'
